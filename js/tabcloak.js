c745a0950df27bbcf9a32524be7a8111c5ed7ff3
ßrunys <realbrunys@gmail.com>
2024-06-01 13:30:56 -0400

    Update tabcloak.js

--- js/tabcloak.js
+++ js/tabcloak.js
@@ -1,14 +1,9 @@
-document.getElementById('changeBtn').addEventListener('click', function() {
-    
+function changeFaviconAndTitle(newFaviconPath, newTitle) {
     var favicon = document.getElementById('favicon');
-    var newFavicon = document.createElement('link');
-    newFavicon.rel = 'icon';
-    newFavicon.href = '/images/brunysixl.png';
-    newFavicon.type = 'image/png';
+    favicon.href = newFaviconPath;
+    document.title = newTitle;
+}
 
-    
-    favicon.parentNode.replaceChild(newFavicon, favicon);
-
-    
-    document.title = 'BrunysIXL';
+document.getElementById('changeBtn').addEventListener('click', function() {
+    changeFaviconAndTitle('new-favicon.png', 'New Title');
 });
