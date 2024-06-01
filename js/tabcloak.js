deb9e0f9864b9992f24dde8b7b90877708659296
ßrunys <realbrunys@gmail.com>
2024-06-01 13:18:58 -0400

    Update tabcloak.js

--- js/tabcloak.js
+++ js/tabcloak.js
@@ -1,12 +1,8 @@
-function changeFaviconAndTitle(newFaviconPath, newTitle) {
-  
-    var favicon = document.getElementById('favicon');
-    favicon.href = newFaviconPath;
-
-  
-    document.title = newTitle;
-}
-
 document.getElementById('changeBtn').addEventListener('click', function() {
-    changeFaviconAndTitle('/images/brunysixl.png', 'BrunysIXL');
+   
+    var favicon = document.getElementById('favicon');
+    favicon.href = '/images/brunysixl.png';
+
+    
+    document.title = 'BrunysIXL';
 });
