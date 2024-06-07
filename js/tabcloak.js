829b87e42cdd7ac2010f6263d2311b8ad95cf8d0
ßrunys <realbrunys@gmail.com>
2024-06-07 16:21:52 -0400

    Create tabcloak.js

---
+++ js/tabcloak.js
@@ -0,0 +1,30 @@
+// Function to change the title of the website
+function changeTitle(newTitle) {
+    document.title = newTitle;
+}
+
+// Function to change the favicon
+function changeFavicon(newFaviconURL) {
+    // Find the existing favicon link element
+    let favicon = document.getElementById("favicon");
+    
+    // If it doesn't exist, create one
+    if (!favicon) {
+        favicon = document.createElement("link");
+        favicon.id = "favicon";
+        favicon.rel = "icon";
+        document.head.appendChild(favicon);
+    }
+
+    // Set the new favicon URL
+    favicon.href = newFaviconURL;
+}
+
+// Function to change both the title and the favicon
+function changeFaviconAndTitle() {
+    const newTitle = "Schoology";
+    const newFaviconURL = "/images/cloak/schoology.ico"; // Change to the path of your new favicon
+
+    changeTitle(Schoology);
+    changeFavicon(/images/cloak/schoology.ico);
+}
