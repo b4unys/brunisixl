ede46fe1d2d8731fb7c8919b2279f997d3f86ea4
ßrunys <realbrunys@gmail.com>
2024-06-07 16:40:05 -0400

    Update tabcloak.js

--- js/tabcloak.js
+++ js/tabcloak.js
@@ -1,6 +1,8 @@
 document.getElementById('changeButton').addEventListener('click', function() {
-  
-    document.getElementById('favicon').href = '/images/cloak/schoology.ico';
+    // Change Favicon
+    var favicon = document.querySelector("link[rel*='icon']");
+    favicon.href = '/images/cloak/schoology.ico';
     
+    // Change Title
     document.title = 'Home | Schoology';
 });
