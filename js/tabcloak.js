f8eb3e3cd230a652d3f6b1ce5ca03af1b1fc3622
ßrunys <realbrunys@gmail.com>
2024-06-07 16:42:56 -0400

    Update tabcloak.js

--- js/tabcloak.js
+++ js/tabcloak.js
@@ -1,8 +1,22 @@
-document.getElementById('changeButton').addEventListener('click', function() {
-    // Change Favicon
-    var favicon = document.querySelector("link[rel*='icon']");
-    favicon.href = '/images/cloak/schoology.ico';
-    
-    // Change Title
-    document.title = 'Home | Schoology';
+document.addEventListener('DOMContentLoaded', function() {
+    // Check if there are stored favicon and title
+    if(sessionStorage.getItem('favicon')) {
+        var favicon = document.querySelector("link[rel*='icon']");
+        favicon.href = sessionStorage.getItem('favicon');
+    }
+    if(sessionStorage.getItem('title')) {
+        document.title = sessionStorage.getItem('title');
+    }
+
+    // Change Favicon and Title
+    document.getElementById('changeButton').addEventListener('click', function() {
+        // Change Favicon
+        var favicon = document.querySelector("link[rel*='icon']");
+        favicon.href = 'new_favicon.png';
+        sessionStorage.setItem('favicon', '/images/cloak/schoology.ico');
+        
+        // Change Title
+        document.title = 'New Title';
+        sessionStorage.setItem('title', 'Home | Schoology');
+    });
 });
