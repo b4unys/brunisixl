61d7cbf3def40d763303c5b652fdff391d065775
ßrunys <realbrunys@gmail.com>
2024-06-07 16:18:26 -0400

    Delete js/tabcloak.js

--- js/tabcloak.js
+++
@@ -1,9 +0,0 @@
-function changeFaviconAndTitle(newFaviconPath, newTitle) {
-    var favicon = document.getElementById('favicon');
-    favicon.href = newFaviconPath;
-    document.title = newTitle;
-}
-
-document.getElementById('changeBtn').addEventListener('click', function() {
-    changeFaviconAndTitle('new-favicon.png', 'New Title');
-});
