680797a1869b6a9c37445471e7cf1eda9ce1bbf4
ßrunys <realbrunys@gmail.com>
2024-06-07 17:24:12 -0400

    Update tabcloak.js

--- js/tabcloak.js
+++ js/tabcloak.js
@@ -1,46 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var isToggled = false; 
+    var isToggled = false;
 
-    
+    function setFavicon(iconURL) {
+        let link = document.querySelector("link[rel~='icon']");
+        if (!link) {
+            link = document.createElement('link');
+            link.rel = 'icon';
+            document.getElementsByTagName('head')[0].appendChild(link);
+        }
+        link.href = iconURL;
+    }
+
     function applyChanges() {
-
-        var favicon = document.querySelector("link[rel*='icon']");
-        favicon.href = '/images/schoologyfavicon.png';
+        setFavicon('/images/schoologyfavicon.png');
         localStorage.setItem('favicon', '/images/schoologyfavicon.png');
-        
-        
-        document.title = 'Home | Schoology';
+        document.title = 'New Title';
         localStorage.setItem('title', 'Home | Schoology');
     }
 
-  
     function removeChanges() {
-
-        var favicon = document.querySelector("link[rel*='icon']");
-        favicon.href = '/images/cloak/brunysixlfavicon.png';
+        setFavicon('/images/brunysixlfavicon.png');
         localStorage.removeItem('favicon');
-        
-
         document.title = 'IXL | Math';
         localStorage.removeItem('title');
     }
 
-  
     document.getElementById('toggleButton').addEventListener('click', function() {
         if (isToggled) {
-            
             removeChanges();
             isToggled = false;
         } else {
-
             applyChanges();
             isToggled = true;
         }
     });
 
-    
     if(localStorage.getItem('favicon') && localStorage.getItem('title')) {
-        applyChanges();
+        setFavicon(localStorage.getItem('favicon'));
+        document.title = localStorage.getItem('title');
         isToggled = true;
     }
 });
